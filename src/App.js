11742ec7767dc6237db2fff977da508d9b40482a
Roman Diviš <divisr@email.cz>
2017-08-15 12:09:52 +0200

    Show first rows od dummy data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const baseHour = 8
+const durationOfWorkDay = 8
+
+// props = {fn click, user, hour, state}
+const Cell = (props) => (
+  <td className={props.state} onClick={e => props.click(props)}>{props.state}</td>
+)
+
+// props = {user, day, data[...,...,...]}
+class UserTableRow extends Component {
+  render() {
+    const data = this.props.data || []
+    const cells = data.map((state, idx) => (
+      <Cell key={idx} user={this.props.user} day={this.props.day} hour={baseHour + idx} state={state}/>
+    ))
+
+    return (
+      <tr>
+        <td>{this.props.user}</td>
+        {cells}
+      </tr>
+    )
+  }
+}
+
+
+
 class TableHeaderWithHours extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = {};
-
-    this.click = this.click.bind(this);
+    //this.state = {};
+    //this.click = this.click.bind(this);
   }
 
-  click(e) {
+  /*click(e) {
     let key = e.target.attributes.getNamedItem('data-key').value;
     this.setState(prevState => ({
       [key]: !prevState[key]
@@ -21,16 +47,20 @@ class TableHeaderWithHours extends Component {
     this.setState(prevState => ({
       [i]: !prevState[i]
     }));
-  }
+  }*/
 
   render() {
-    let ths = this.props.hours.map(hour => (
-      this.state[hour] ?
-        <th className='red' onClick={this.click} key={hour} data-key={hour}>{hour}-{hour + 1}</th>
-        :
-        <th onClick={(e) => {this.click2(e, hour);}} key={hour} data-key={hour}>{hour}-{hour + 1}</th>
+    const ths = this.props.hours.map(hour => (
+      <th key={hour}>{hour}-{hour + 1}</th>
+    ))
 
-    ));
+    /*let ths = this.props.hours.map(hour => (
+      this.state[hour] ? // onClick={this.click}  onClick={(e) => {this.click2(e, hour);}}
+        <th className='red' key={hour} data-key={hour}>{hour}-{hour + 1}</th>
+        :
+        <th key={hour} data-key={hour}>{hour}-{hour + 1}</th>
+
+    ));*/
 
     return (
       <tr>
@@ -41,31 +71,76 @@ class TableHeaderWithHours extends Component {
   }
 }
 
-class Tabulka extends Component {
+class DayGroup extends Component {
   render() {
+    const userRowsCount = Object.keys(this.props.users).length
+    const userRows = Object.keys(this.props.users).map(user => (
+      <UserTableRow key={user} user={user} day={this.props.day} data={this.props.users[user]} />
+    ))
+
+    return (
+      <tbody>
+        <tr><td rowSpan={userRowsCount+2}>{this.props.day}</td></tr>
+        {userRows}
+        <tr></tr>
+      </tbody>
+    )
+  }
+}
+
+class Tabulka extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  aggregateUserDataForDay(day) {
+    return this.props.data.reduce((result, userObject) => {
+      result[userObject.user] = userObject.data[day]
+      return result
+    }, {})
+  }
+
+  render() {
+    const days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"].map(day => (
+        <DayGroup key={day} day={day} users={this.aggregateUserDataForDay(day)} />
+    ))
+
     return (
       <table>
         <thead>
           <TableHeaderWithHours hours={this.props.hours}/>
         </thead>
-        <tbody>
-          <tr><td>aa</td></tr>
-        </tbody>
+        {days}
       </table>
-    );
+    )
   }
 }
 
 class App extends Component {
   render() {
-    const hours = [8,9,10,11,12,13,14,15,16];
+    const hours = [8,9,10,11,12,13,14,15,16]
+    const data = [
+      {
+        user: "katka",
+        data: {
+          monday: ["free", "free", "occupied", "maybe", "free", "free", "free", "free", "occupied"],
+          tuesday: ["free", "free", "occupied", "maybe", "free", "free", "free", "free", "maybe"]
+        }
+      },
+      {
+        user: "petra",
+        data: {
+          monday: ["occupied", "occupied", "occupied", "maybe", "free", "free", "free", "free", "free"],
+          tuesday: ["free", "free", "occupied", "maybe", "free", "free", "free", "free", "free"]
+        }
+      }
+    ]
+
 
     return (
       <div className="App">
-        <Tabulka hours={hours}/>
+        <Tabulka hours={hours} data={data}/>
       </div>
-
-
     );
   }
 }
