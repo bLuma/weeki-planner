64594bf7aba5884db8a411c81b2170697169a29b
Roman Diviš <divisak@gmail.com>
2017-10-11 18:39:36 +0200

    Implemented base week calendar loading

--- backend/app/Http/Controllers/CalendarController.php
+++ backend/app/Http/Controllers/CalendarController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Calendar;
+use DateTime;
+use DateInterval;
 
 class CalendarController extends Controller
 {
@@ -17,11 +19,76 @@ class CalendarController extends Controller
         $this->middleware('auth');
     }
 
+    private function fixDayOfWeek($dayOfWeek) {
+      $newDayOfWeek = $dayOfWeek - 1;
+      if ($newDayOfWeek < 0) {
+          $newDayOfWeek = 7;
+      }
+
+      return $newDayOfWeek;
+    }
+
+    private function getWeekdayName($dayOfWeek) {
+      switch ($dayOfWeek) {
+        case 0: return "monday";
+        case 1: return "tuesday";
+        case 2: return "wednesday";
+        case 3: return "thursday";
+        case 4: return "friday";
+        case 5: return "saturday";
+        case 6: return "sunday";
+      }
+    }
+
     public function req(Request $request)
     {
-        $out = $request->user()->calendar()->get();
-        return response($out);
-//      return response()->json(["woho" => "wooo"]);
+        $timestamp = time();
+
+        // date: 0 sunday, 1 monday, ...
+        // after fixing: 0 monday, 1 tuesday, ...
+        $dayOfWeek = $this->fixDayOfWeek(date("w", $timestamp));
+
+        // week number - starts with monday
+        $weekOfYear = date("W", $timestamp);
+        $oddOrEvenWeek = $weekOfYear % 2 == 0 ? 'e' : 'o';
+
+        $dateTime = new DateTime();
+        $dateTime->setTimestamp($timestamp);
+        $dateTime->setTime(0, 0);
+
+        $mondayDateTime = clone $dateTime;
+        $mondayDateTime->sub(new DateInterval('P'.$dayOfWeek.'D'));
+
+        $nextMondayDateTime = clone $mondayDateTime;
+        $nextMondayDateTime->add(new DateInterval('P7D'));
+
+        $check = date(DATE_ATOM, $timestamp);
+        echo "<pre>";
+        var_dump([
+            "dayOfWeek" => $dayOfWeek,
+            "weekOfYear" => $weekOfYear,
+            "oddOrEvenWeek" => $oddOrEvenWeek,
+            "check" => $check,
+            "dateTime" => $dateTime,
+            "mondayDateTime" => $mondayDateTime,
+            "nextMondayDateTime" => $nextMondayDateTime
+        ]);
+
+        $result = [];
+        for ($weekDay = 0; $weekDay < 7; $weekDay++) {
+          $result[$this->getWeekdayName($weekDay)] = [];
+        }
+
+        $userCalendar = $request->user()->calendar();
+
+        $baseCalendar = $userCalendar->where('week', $oddOrEvenWeek)->get();
+        foreach ($baseCalendar as $record) {
+          $result[$this->getWeekdayName($record->day)][$record->hour] = ["state" => $record->state, "comment" => $record->comment, "type" => "base"];
+        }
+
+        print_r($result);
+
+        return response()->json($result);
     }
 
     //
--- backend/database/seeds/CalendarsTableSeeder.php
+++ backend/database/seeds/CalendarsTableSeeder.php
@@ -13,7 +13,7 @@ class CalendarsTableSeeder extends Seeder {
     $states = ['f', 'm', 'o'];
 
     for($day = 0; $day < 5; $day++) {
-      for ($hour = 8; $hour < 16; $hour++) {
+      for ($hour = 0; $hour < 8; $hour++) {
         for ($week = 0; $week < rand() % 2 + 1; $week++) {
           Calendar::create([
             'user' => $adminId,
@@ -26,5 +26,27 @@ class CalendarsTableSeeder extends Seeder {
         }
       }
     }
+
+    $dt = new DateTime();
+    $dt->setDate($dt->format('Y'), 1, 1);
+    $dt->setTime(0, 0);
+
+    for($day = 0; $day < 365; $day++) {
+      for ($hour = 0; $hour < 8; $hour++) {
+        if (rand() % 100 < 10) {
+          Calendar::create([
+            'user' => $adminId,
+            'day' => $dt->getTimestamp(),
+            'hour' => $hour,
+            'week' => 'c',
+            'state' => $states[rand() % count($states)],
+            'comment' => ''
+          ]);
+        }
+
+        $dt->add(new DateInterval('P1D'));
+      }
+    }
   }
+
 }
